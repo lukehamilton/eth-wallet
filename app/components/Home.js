765f75eb2ae97fbe54a5fecfde397218b44d822c
Luke Hamilton <lukehamiltonmail@gmail.com>
2017-05-27 17:37:40 -0400

    Listing accounts

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,15 +2,20 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Home.css';
+import Web3 from 'web3';
 
 export default class Home extends Component {
 
   constructor(props)  {
     super(props)
+    this.state = {
+      web3: new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+    }
   }
 
   generateAddress() {
     console.log("Generating address")
+    console.log('Web 3', Web3);
   }
 
   render() {
@@ -20,6 +25,12 @@ export default class Home extends Component {
           <h2>Ether Wallet</h2>
           {/* <Link to="/counter">to Counter</Link> */}
           <a onClick={this.generateAddress}>Generate New Address</a>
+          <h4>Accounts on Test Net</h4>
+          { this.state.web3.eth.accounts.map(account =>{
+            return (
+              <div>{ account }</div>
+            )
+          })}
         </div>
       </div>
     );
