88b3345dd638088ad633bcce203f5c0dd07692c9
Luke Hamilton <lukehamiltonmail@gmail.com>
2017-05-27 17:23:49 -0400

    first button

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,12 +4,22 @@ import { Link } from 'react-router-dom';
 import styles from './Home.css';
 
 export default class Home extends Component {
+
+  constructor(props)  {
+    super(props)
+  }
+
+  generateAddress() {
+    console.log("Generating address")
+  }
+
   render() {
     return (
       <div>
         <div className={styles.container} data-tid="container">
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
+          <h2>Ether Wallet</h2>
+          {/* <Link to="/counter">to Counter</Link> */}
+          <a onClick={this.generateAddress}>Generate New Address</a>
         </div>
       </div>
     );
